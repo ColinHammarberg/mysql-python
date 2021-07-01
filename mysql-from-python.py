3abf0366f4c6e11f79ba00fada55df50fbbb1439
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-07-01 22:13:57 +0000

    Added some SQL data

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,8 +12,13 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 try:
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age = %s WHERE name = %s;",
-                       (23, 'bob'))
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+
         connection.commit()
 finally:
     connection.close()
